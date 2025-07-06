a037a838ba19525329e6670390d777b2c69b7f90
priyanshu shrivastava <108760695+priyanshu334@users.noreply.github.com>
2025-07-06 20:05:13 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["example.com", "fra.cloud.appwrite.io"],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
